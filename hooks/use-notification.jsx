b2a7d25ca79e78a70d70f7873f21f1ef1e71bae3
unknown <jihadkhan934@gmail.com>
2024-08-22 00:02:26 +0600

    user manually close button are work

--- hooks/use-notification.jsx
+++ hooks/use-notification.jsx
@@ -1,36 +1,59 @@
-import React from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import Notification from "../components/notification";
 
-
 const divPosition = {
-    "top-left": "top-2 fixed z-50",
-    "bottom-left": "fixed z-50 left-2 bottom-2",
-    "top-right": "right-2 fixed z-50 top-2",
-    "bottom-right": "right-2 fixed z-50 bottom-2",
-    "top-center": "top-2 fixed z-50 left-1/2 -translate-x-1/2",
-    "bottom-center": "bottom-2 fixed z-50 left-1/2 -translate-x-1/2",
+  "top-left": "top-2 fixed z-50",
+  "bottom-left": "fixed z-50 left-2 bottom-2",
+  "top-right": "right-2 fixed z-50 top-2",
+  "bottom-right": "right-2 fixed z-50 bottom-2",
+  "top-center": "top-2 fixed z-50 left-1/2 -translate-x-1/2",
+  "bottom-center": "bottom-2 fixed z-50 left-1/2 -translate-x-1/2",
 };
 
 const useNotification = (position = "top-right") => {
-  const [notification, setNotification] = React.useState(null);
+  const [notification, setNotification] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const [timer, setTimer] = useState(null);
 
+  const triggerNotification = useCallback((props) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
 
-  let timer ;
-  const triggerNotification = (porps) => {
-    clearTimeout(timer);
-    setNotification(porps);
+    setNotification(props);
+    setIsVisible(true);
+
+    const newTimer = setTimeout(() => {
+      setIsVisible(false);
+      setTimeout(() => {
+        setNotification(null);
+      }, 300);
+    }, props.duration || 1000);
+
+    setTimer(newTimer);
+  }, [timer]);
+
+  const handleClose = () => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    setIsVisible(false);
     setTimeout(() => {
       setNotification(null);
-    }, porps.duration || 1000);
+    }, 300);
   };
 
   const NotificationComponent = notification ? (
-    <div className={`${divPosition[position]}`}>
-      <Notification {...notification} />
+    <div
+      className={`${divPosition[position]} transition-all duration-300 ${
+        isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
+      }`}
+    >
+      <Notification {...notification} onClose={handleClose} />
     </div>
   ) : null;
 
-  return {NotificationComponent, triggerNotification};
+  return { NotificationComponent, triggerNotification };
 };
 
 export default useNotification;
